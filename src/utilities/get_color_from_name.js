fc0c3703dcb244f9df9dd750e21c0555018cad4a
Gregor Maclaine <thegregormaclaine@gmail.com>
2022-01-08 04:52:12 +0000

    'Obfuscated' the get_color_from_name algorithm

--- src/utilities/get_color_from_name.js
+++ src/utilities/get_color_from_name.js
@@ -7,7 +7,16 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
  * A username should always point to the same colour for consistency.
  */
 export default function main(username, id) {
-  // Get unique(ish) number for username
-  const code = (username + id).split('').reduce((acc, cur, i) => acc + Math.max(alphabet.indexOf(cur), 0) * i, 0);
+  // Get unique array of characters from combination of username & socket id
+  const unique_id = username + id;
+  const characters = unique_id.split('');
+
+  // Caculated a number based on the characters in the unique string and their placement
+  const code = characters.reduce((acc, cur, i) => {
+    const addition = Math.max(alphabet.indexOf(cur) + 1, 0) * i
+    return acc + addition;
+  }, 0);
+
+  // Find a viable colour code depending on this caculation
   return colors[code % colors.length];
 }
