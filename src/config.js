3effffbf4d19f64400c1ffa0469fa9c6b013aeed
Gregor Maclaine <thegregormaclaine@gmail.com>
2022-02-19 04:08:11 +0000

    Changed env to react app one

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 export const THEME = {
   blue: '#01295f',
   lightblue: '#437f97',
@@ -6,5 +8,5 @@ export const THEME = {
   red: '#fd151b'
 };
 
-export const SERVER_ADDRESS = process.env.SERVER_ADDRESS || `http://${window.location.hostname || 'localhost'}:3001`;
+export const SERVER_ADDRESS = process.env.REACT_APP_SERVER_ADDRESS || `http://${window.location.hostname || 'localhost'}:3001`;
 console.log(SERVER_ADDRESS)
